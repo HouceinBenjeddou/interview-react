a8bbc58e386cb74771ca44b2b652254a9fb3953d
HouceinBenjeddou <hou52cine@gmail.com>
2023-01-23 23:17:03 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css';
-import Heading from './Components/context/Heading';
-import Section from './Components/context/Section';
+import File from './Components/context/File';
 function App() {
 
   return (
     <div className="App">
-   <Section>
-      <Heading>Title</Heading>
-      <Section>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Section>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Section>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-          </Section>
-        </Section>
-      </Section>
-    </Section>
+   <File d/>
     </div>
   );
 }
